f63194082160dd43db52d7a765c0296dcc400709
Taki <phantuankiet91@gmail.com>
2023-08-02 19:09:39 +0700

    Refactor Project Structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
+import Board from './pages/Boards/_id'
 
 function App() {
 
   return (
     <>
-      <div>test dev</div>
-      <Button variant="text">text</Button>
-      <Button variant="contained">Hello World</Button>
-      <Button variant="outlined">outlined</Button>
-      <br />
-      <AccessAlarmIcon />
-      <ThreeDRotation />
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[700] }} />
-
+    {/* Board detail */}
+      <Board />
     </>
   )
 }
