6f955b902c3a4162538fda9c314134a28ef45966
Taki <phantuankiet91@gmail.com>
2023-08-11 23:06:59 +0700

    update 11/8 2

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -10,7 +10,7 @@ function Board() {
     <Container disableGutters maxWidth={false} sx={{ height: '100vh', backgroundColor:'primary.main' }}>
       <AppBar />
       <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      {/* <BoardContent board={mockData?.board} /> */}
       <BoardTaskContent boards={mockData?.boards} />
     </Container>
   )
